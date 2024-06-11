e925ef498172c5ebb5d75d2d267d3238ec949eec
Prasanta Roy <prasanta37029@gmail.com>
2024-06-11 21:34:11 +0530

    Update the searchview page

--- app/src/main/java/com/pkyr/brainace/SearchViewActivity.java
+++ app/src/main/java/com/pkyr/brainace/SearchViewActivity.java
@@ -1,5 +1,6 @@
 package com.pkyr.brainace;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -16,9 +17,16 @@ import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pkyr.brainace.adapters.AssignmentAdapter;
 import com.pkyr.brainace.databinding.ActivitySearchViewBinding;
 import com.pkyr.brainace.model.AssignmentModel;
+import com.pkyr.brainace.model.SubjectModel;
+import com.pkyr.brainace.utils.FirebaseUtils;
 
 import java.util.ArrayList;
 
@@ -26,6 +34,8 @@ public class SearchViewActivity extends AppCompatActivity {
 
     private ActivitySearchViewBinding binding;
     private AssignmentAdapter adapter;
+    private ArrayList<AssignmentModel> assignmentList;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +52,83 @@ public class SearchViewActivity extends AppCompatActivity {
         imm.showSoftInput(binding.searchActivitySearchView, InputMethodManager.SHOW_IMPLICIT);
 
 
-        RecyclerView recyclerView = binding.searchActivityRecyclerView;
+        recyclerView = binding.searchActivityRecyclerView;
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), LinearLayout.VERTICAL));
 
-//        adapter = new AssignmentAdapter(this, new ArrayList<AssignmentModel>());
-//        recyclerView.setAdapter(adapter);
-
+        assignmentList = new ArrayList<>();
+        adapter = new AssignmentAdapter(this, assignmentList);
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        loadAllAssignments(assignmentList);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    private void loadAllAssignments(ArrayList<AssignmentModel> assignmentList) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = database.getReference();
+
+        DatabaseReference assignmentRef = dbRef.child("bwu")
+                .child(MainActivity.userModel.getCourse())
+                .child(MainActivity.userModel.getBatch())
+                .child(MainActivity.userModel.getSem())
+                .child(MainActivity.userModel.getSec())
+                .child("subjects");
+        // get the all subjects and find the assignments
+        assignmentRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                        SubjectModel subjectModel = snapshot1.getValue(SubjectModel.class);
+                        loadAssignmentInSubject(subjectModel.getSubjectName(), assignmentList);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void loadAssignmentInSubject(String subjectName, ArrayList<AssignmentModel> assignmentList) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = database.getReference();
+
+        DatabaseReference aRef = dbRef.child("bwu")
+                .child(MainActivity.userModel.getCourse())
+                .child(MainActivity.userModel.getBatch())
+                .child(MainActivity.userModel.getSem())
+                .child(MainActivity.userModel.getSec())
+                .child("subjects")
+                .child(subjectName)
+                .child("assignments");
+
+        aRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    for(DataSnapshot snapshot1 : snapshot.getChildren()) {
+                        AssignmentModel a = snapshot1.getValue(AssignmentModel.class);
+                        assignmentList.add(a);
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    };
 }
--- app/src/main/java/com/pkyr/brainace/utils/FirebaseUtils.java
+++ app/src/main/java/com/pkyr/brainace/utils/FirebaseUtils.java
@@ -1,9 +1,23 @@
 package com.pkyr.brainace.utils;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.pkyr.brainace.MainActivity;
+import com.pkyr.brainace.adapters.AssignmentAdapter;
+import com.pkyr.brainace.model.AssignmentModel;
+import com.pkyr.brainace.model.SubjectModel;
+import com.pkyr.brainace.model.UserModel;
+
+import java.util.ArrayList;
 
 public class FirebaseUtils {
 
@@ -22,4 +36,5 @@ public class FirebaseUtils {
     public static CollectionReference getAllUserDetails() {
         return FirebaseFirestore.getInstance().collection("users");
     }
+
 }
